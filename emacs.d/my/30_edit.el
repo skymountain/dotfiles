0c77d91579eef09a3d3d6890a6700c80cb610acd
skymountain <ryukilon0919@gmail.com>
2010-12-05 02:36:44 +0900

    move 30_search.el to 30_edit.el

--- emacs.d/my/30_edit.el
+++ emacs.d/my/30_edit.el
@@ -72,3 +72,13 @@ Strings matched with STRING in template file are replaced by REPLACE, which is s
 ; diff
 (load "diff-with-original")
 (load "ediff-with-original")
+
+; isearch-grep
+(require 'isearch-grep)
+
+; moccur
+(require 'color-moccur)
+(require 'moccur-edit)
+(custom-set-variables
+ '(moccur-split-word t)
+ )
