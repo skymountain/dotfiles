d2675ba1829356e51556001fe40efaddc77ab664
skymountain <ryukilon0919@gmail.com>
2011-01-13 21:32:46 +0900

    fix functions

--- emacs.d/my/00_func.el
+++ emacs.d/my/00_func.el
@@ -3,9 +3,12 @@
     (setq acc (funcall f acc x))))
 
 (defun foldr (f xs acc)
-  (if xs
-      (f (car xs) (foldr (cdr xs) acc))
-      acc))
+  (if (null xs) acc
+    (funcall f (car xs) (foldr f (cdr xs) acc))))
+
+
+(defun filter (my-filter-function xs)
+  (foldr (lambda (x acc) (if (funcall my-filter-function x) (cons x acc) acc)) xs nil))
 
 (defun string-match-last (r s &optional offset)
   (let ((idx (string-match r s offset)))
