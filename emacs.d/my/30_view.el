c8f107f3c7bb548d8a47893c2739d35fb1c4d275
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2011-01-15 01:17:19 +0900

    add a function which describe face at the cursor point

--- emacs.d/my/30_view.el
+++ emacs.d/my/30_view.el
@@ -10,3 +10,9 @@
 ; show the word `lambda' as the Greek letter
 (require 'pretty-lambdada)
 (pretty-lambda-for-modes)
+
+; describe face
+(defun describe-face-at-point ()
+  "Show a face at the point of the current cursor"
+  (interactive)
+  (message "%s" (get-char-property (point) 'face)))
