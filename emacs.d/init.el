2fb7d8f37e93eb698c8da381263406b755dc4bb2
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2011-01-14 17:44:29 +0900

    .elc have higher priority than .el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,8 +8,23 @@
   '(init-loader-show-log-after-init nil)
   '(init-loader-default-regexp "\\(?:^[[:digit:]]\\{2\\}\\).*\\.elc?$")
   )
+
 (defadvice init-loader-error-log (before enable-showing-init-loader-error-log activate)
   (custom-set-variables
     '(init-loader-show-log-after-init t)
     ))
+
+(defadvice init-loader--re-load-files
+  (after init-loader--re-load-files-filter activate)
+  (let* ((filter (lambda (func xs)
+                   (let ((tmp-list ()))
+                     (dolist (x xs (reverse tmp-list))
+                       (when (funcall func x)
+                         (setq tmp-list (cons x tmp-list)))))))
+         (el-with-elc
+          (mapcar (lambda (el) (replace-regexp-in-string "\\.elc$" ".el" el t))
+                  (funcall filter (lambda (el) (string-match "\\.elc$" el))
+                           ad-return-value))))
+    (setq ad-return-value (funcall filter (lambda (el) (not (member el el-with-elc))) ad-return-value))))
+
 (init-loader-load (expand-file-name "~/.emacs.d/my"))
