e1d53d5b8690cfcf57642f0b5dfb334a614f1b8e
skymountain <ryukilon@gmail.com>
2011-03-27 07:08:03 +0900

    modify to how to specify local color-theme

---
+++ emacs.d/my/99_final.el
@@ -0,0 +1,7 @@
+; color theme
+(require 'color-theme)
+(add-to-list 'color-themes '(my-color-theme  "TARO SEKIYAMA" "color-theme-sky-gray"))
+(color-theme-initialize)
+(color-theme-install (my-color-theme))
+
+
