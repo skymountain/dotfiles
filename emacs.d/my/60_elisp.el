8eecc4d08b4b43ee181370cea2d85c666014a1c6
skymountain <ryukilon@gmail.com>
2011-03-27 22:42:53 +0900

    add keybindings in elisp mode

--- emacs.d/my/60_elisp.el
+++ emacs.d/my/60_elisp.el
@@ -2,4 +2,10 @@
 (add-hook 'lisp-interaction-mode-hook
           (lambda ()
             (global-keybinds-define-keybinds lisp-interaction-mode-map)
+            (define-keybinds lisp-interaction-mode-map
+              '(("C-M-h f" describe-function)
+                ("C-M-h v" describe-variable)
+                ("C-M-h b" describe-bindings)
+                ("C-M-h k" describe-key)
+                ))
             ))
