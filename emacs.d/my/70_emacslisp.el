f256944f3337bcd862eba82eeb82f6d66964daf5
skymountain <ryukilon@gmail.com>
2011-08-02 17:13:43 +0900

    add package name and url to elisp

--- emacs.d/my/70_emacslisp.el
+++ emacs.d/my/70_emacslisp.el
@@ -25,12 +25,17 @@
      "color-moccur.el"
      "moccur-edit.el"
      "anything-gtags.el"
+     "ipa.el"
      ))
   (install-elisp-list-from-url
    '("http://www.wonderworks.com/download/filladapt.el"
      "http://people.debian.org/~psg/elisp/font-latex.el"
      "http://www.gentei.org/~yuuji/software/windows.el"
-     "http://svn.coderepos.org/share/lang/elisp/anything-c-moccur/trunk/anything-c-moccur.el"))
+     "http://www.gentei.org/~yuuji/software/revive.el"
+     "http://svn.coderepos.org/share/lang/elisp/anything-c-moccur/trunk/anything-c-moccur.el"
+     "http://www.eecs.ucf.edu/~leavens/emacs/camelCase/camelCase-mode.el"
+     "http://coderepos.org/share/export/38971/lang/elisp/init-loader/init-loader.el"
+     ))
   (install-elisp-list-by-batch
    '("auto-complete"
      "anything")))
