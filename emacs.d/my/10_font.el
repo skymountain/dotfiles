c632371044cea0e54bb4a10308d219e1f8256615
skymountain <ryukilon@gmail.com>
2011-08-09 05:09:08 +0900

    move font settings to local file

--- emacs.d/my/10_font.el
+++
@@ -1,2 +0,0 @@
-(custom-set-faces
- '(default ((t (:inherit nil :stipple nil :background "Grey15" :foreground "Grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "microsoft" :family "Consolas")))))
