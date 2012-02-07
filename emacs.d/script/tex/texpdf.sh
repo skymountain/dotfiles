64c7b1ccb30febc3613e245456bac4911ec1eace
skymountain <ryukilon@gmail.com>
2012-02-08 01:56:37 +0900

    add generation of tex from ott

--- emacs.d/script/tex/texpdf.sh
+++ emacs.d/script/tex/texpdf.sh
@@ -1,7 +1,61 @@
 #!/bin/bash
 
-file=$1
-dvifile=${file%.*}.dvi
-psfile=${file%.*}.ps
+function filter-tex {
+  ott=$1
+  tex=$2
+  suffix=$3
 
-platex $file && platex $file && dvips -P pdf $dvifile && ps2pdf $psfile
+  input=${ott%.*}.tex
+
+  echo "OTT FROM: $tex TO: ${tex%.*}$suffix"
+
+  ott                    \
+    -i $ott              \
+    -o $input            \
+    -tex_show_meta false \
+    -tex_name_prefix ott \
+    -tex_wrap false      \
+    -tex_filter "$tex" "${tex%.*}$suffix"
+
+  return $?
+}
+
+function bib {
+  res=0
+
+  gres=`grep 'bibliography' "$1.tex" 2> /dev/null` 2> /dev/null
+
+  if [ "$gres" ]; then
+    echo "COMPILE BIBTEX: $1 ..."
+    bibtex $1
+    res=$?
+  fi
+
+  if [ $res -ne 0 ]; then
+    echo "BIB COMPILE WAS FAILED!"
+  else
+    echo "BIB COMPILE WAS SUCCESS!"
+  fi
+
+  return $res
+}
+
+file=$1
+dir=$PWD
+ott=$dir/*.ott
+
+if [ -f $ott ]; then
+  suffix='.ott.tex'
+  filter-tex $ott $file $suffix
+  if [ $? -ne 0 ]; then
+    exit $?
+  fi
+
+  file=${file%.*}$suffix
+fi
+
+base=${file%.*}
+dvifile=$base.dvi
+psfile=$base.ps
+
+platex $file && bib $base && platex $file && platex $file && dvips -P pdf $dvifile && ps2pdf $psfile
