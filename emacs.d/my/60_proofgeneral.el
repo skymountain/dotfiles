f1621adc7d35c0954a839b5a45c0964ec935ee79
skymountain <ryukilon@gmail.com>
2012-06-29 10:35:52 +0900

    add proof mode

---
+++ emacs.d/my/60_proofgeneral.el
@@ -0,0 +1,19 @@
+(require 'proof-site)
+
+(add-to-list 'ac-modes 'coq-mode)
+
+(custom-set-variables
+ '(proof-electric-terminator-enable t)
+ )
+
+(add-hook 'proof-mode-hook
+          (lambda ()
+            (global-keybinds-define-keybinds proof-mode-map)
+            (define-keybinds proof-mode-map
+              '(("M-e" proof-goto-command-end)
+                ("M-a" proof-goto-command-start)
+                ("C-c C-p" proof-process-buffer)
+                ("C-c p" proof-prf)
+                ("C-c RET" proof-goto-point)
+                ))
+            ))
