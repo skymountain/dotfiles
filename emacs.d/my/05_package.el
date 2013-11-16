c37c2c884beebb1abd510f1cb82256b8f2fbe297
skymountain <ryukilon@gmail.com>
2013-11-16 22:48:11 +0900

    update the package list

--- emacs.d/my/05_package.el
+++ emacs.d/my/05_package.el
@@ -6,3 +6,30 @@
      ))
  )
 (package-initialize)
+
+(require 'cl)
+(mapcar (lambda (p)
+          (when (not (package-installed-p p))
+            (package-install p)))
+        '(anything
+          auto-complete
+          color-moccur
+          color-theme
+          flymake-cursor
+          gtags
+          haskell-mode
+          mic-paren
+          perl-completion
+          point-undo
+          popup
+          popwin
+          pretty-lambdada
+          revive
+          rust-mode
+          scheme-complete
+          session
+          shell-pop
+          tuareg
+          undo-tree
+          zlc
+          ))
