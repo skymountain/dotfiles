680d02107cd114a28bfdbc5f6ae5fb63de95bc00
Taro Sekiyama <ryukion@gmail.com>
2016-05-25 16:22:21 +0900

    add an install script for mac

--- script/install-on-mac.sh
+++ script/install-on-mac.sh
@@ -5,11 +5,19 @@ DOTDIR=$DOTDIR/..
 DOTDIR=$(cd $DOTDIR && pwd)
 
 ln -s $DOTDIR/emacs.d $HOME/.emacs.d
-ln -s $DOTDIR/git/gitignore $HOME/.gitignore
+
+git config --global core.excludesfile $DOTDIR/git/gitignore
 ln -s $DOTDIR/misc/aspell.conf $HOME/.aspell.conf
 ln -s $DOTDIR/misc/keysnail.js $HOME/.keysnail.js
+
 ln -s $DOTDIR/screen/screenrc $HOME/.screenrc
 ln -s $DOTDIR/shell/zsh/zshrc $HOME/.zshrc
 
-ln -s $DOTDIR/mac/DefaultKeyBinding.dict $HOME/Library/KeyBindings/
-ln -s $DOTDIR/mac/private.xml "$HOME/Library/Application Support/Karabiner/"
+mkdir -p $HOME/Library/KeyBindings 2> /dev/null
+if [ -e $HOME/Library/KeyBindings/DefaultKeyBinding.dict ]; then
+  cp $HOME/Library/KeyBindings/DefaultKeyBinding.dict $HOME/Library/KeyBindings/DefaultKeyBinding.dict.old
+fi
+cp $DOTDIR/mac/DefaultKeyBinding.dict $HOME/Library/KeyBindings/DefaultKeyBinding.dict
+
+mkdir -p "$HOME/Library/Application Support/Karabiner" 2> /dev/null
+ln -s $DOTDIR/mac/private.xml "$HOME/Library/Application Support/Karabiner"
