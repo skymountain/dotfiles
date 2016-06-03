d02acea70af6981b217970d027c4d38f89e5c492
Taro Sekiyama <ryukion@gmail.com>
2016-06-03 11:47:25 +0900

    add an install script on linux

---
+++ script/install-on-linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DOTDIR=`dirname $0`
+DOTDIR=$DOTDIR/..
+DOTDIR=$(cd $DOTDIR && pwd)
+
+ln -s $DOTDIR/emacs.d $HOME/.emacs.d
+mkdir -p $HOME/.emacs.d/el-get 2> /dev/null
+
+git config --global core.excludesfile $DOTDIR/git/gitignore
+ln -s $DOTDIR/misc/aspell.conf $HOME/.aspell.conf
+ln -s $DOTDIR/misc/keysnail.js $HOME/.keysnail.js
+
+ln -s $DOTDIR/screen/screenrc $HOME/.screenrc
+ln -s $DOTDIR/shell/zsh/zshrc $HOME/.zshrc
+
+echo ''                             >> $HOME/.bash_profile
+echo '# The following lines are inserted by the dot install script' \
+                                    >> $HOME/.bash_profile
+echo "export DOTDIR=$DOTDIR"        >> $HOME/.bash_profile
+echo '. $DOTDIR/shell/bash/profile' >> $HOME/.bash_profile
+echo '. $HOME/.bashrc'              >> $HOME/.bash_profile
+echo '. $DOTDIR/shell/bash/bashrc'  >> $HOME/.bashrc
