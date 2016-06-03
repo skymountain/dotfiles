bdb92cee494c6e62ac86500116579691317958c8
Taro Sekiyama <ryukion@gmail.com>
2016-06-03 11:45:18 +0900

    Enable settings only when proofgeneral is installed

--- emacs.d/my/60_proofgeneral.el
+++ emacs.d/my/60_proofgeneral.el
@@ -1,20 +1,21 @@
-(require 'proof-site)
+(when (require 'proof-site nil 'noerror)
 
-(setq auto-mode-alist
-      (assoc-delete-by-value auto-mode-alist 'hol-light-mode))
+  (setq auto-mode-alist
+        (assoc-delete-by-value auto-mode-alist 'hol-light-mode))
 
-(custom-set-variables
- '(proof-electric-terminator-enable t)
- )
+  (custom-set-variables
+   '(proof-electric-terminator-enable t)
+   )
 
-(add-hook 'proof-mode-hook
-          (lambda ()
-            (global-keybinds-define-keybinds proof-mode-map)
-            (define-keybinds proof-mode-map
-              '(("M-e" proof-goto-command-end)
-                ("M-a" proof-goto-command-start)
-                ("C-c C-p" proof-process-buffer)
-                ("C-c p" proof-prf)
-                ("C-c RET" proof-goto-point)
-                ))
-            ))
+  (add-hook 'proof-mode-hook
+            (lambda ()
+              (global-keybinds-define-keybinds proof-mode-map)
+              (define-keybinds proof-mode-map
+                '(("M-e" proof-goto-command-end)
+                  ("M-a" proof-goto-command-start)
+                  ("C-c C-p" proof-process-buffer)
+                  ("C-c p" proof-prf)
+                  ("C-c RET" proof-goto-point)
+                  ))
+              ))
+)
