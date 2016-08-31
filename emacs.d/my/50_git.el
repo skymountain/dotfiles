a3f181e9723482441ec8daa93cc04ac1a7ae738f
Taro Sekiyama <ryukion@gmail.com>
2016-08-31 23:17:14 +0900

    We mayn't have magit and yatex

--- emacs.d/my/50_git.el
+++ emacs.d/my/50_git.el
@@ -1,9 +1,9 @@
-(require 'magit)
-
-(define-keybinds
-  global-map
-  '(("s" magit-status)
-    ("l" magit-log)
-    ("b" magit-branch-manager)
-    ("p" magit-push))
-  :prefix "C-c g")
+(when (require 'magit nil t)
+  (define-keybinds
+    global-map
+    '(("s" magit-status)
+      ("l" magit-log)
+      ("b" magit-branch-manager)
+      ("p" magit-push))
+    :prefix "C-c g")
+  )
